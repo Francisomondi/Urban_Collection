73ee5db5d1c565469189a20529fb48b2fdecc7d4
Francis omondi <franisomondi17@gmail.com>
2020-05-25 00:28:25 +0300

    home page view design,,ddition of a courosel and abit of styling

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,14 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+const categorySchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    
     date: {
         type: Date,
         default: Date.now(),
     },
 });
 
-const Category = module.exports = mongoose.model("categories", productSchema);
+const Category = module.exports = mongoose.model("categories", categorySchema);
